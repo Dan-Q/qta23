716268c81d23eacdc3d849b44380d37e4c57c320
Dan Q <github@danq.me>
2023-04-18 11:33:50 +0100

    Perform mail deliveries even in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Actually send mail, even in development mode
+  config.action_mailer.perform_deliveries = true
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
