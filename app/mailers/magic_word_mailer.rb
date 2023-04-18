3ffaf049368bac0712a8ba9abd53e137e516a990
Dan Q <github@danq.me>
2023-04-18 11:46:04 +0100

    Fix breaking typo

--- app/mailers/magic_word_mailer.rb
+++ app/mailers/magic_word_mailer.rb
@@ -1,10 +1,11 @@
 class MagicWordMailer < ApplicationMailer
+  default from: ENV['SMTP_USERNAME']
 
   def remind
     @magic_word = params[:magic_word]
     delivery_options = { user_name: ENV['SMTP_USERNAME'],
                          password: ENV['SMTP_PASSWORD'],
-                         address: ENV['SMTP_SERVER'],
+                         address: ENV['SMTP_SERVER'] }
     mail(to: params[:email],
          subject: 'Summer Party at The Green 2023: Magic Word Reminder',
          delivery_method_options: delivery_options)
